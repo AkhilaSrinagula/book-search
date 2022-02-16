b00746dc02fd4f8b156feef3802fe993cc2ea631
Akhila Srinagula <89111745+AkhilaSrinagula@users.noreply.github.com>
2022-02-15 20:19:30 -0600

    added type-defs

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
@@ -19,19 +19,24 @@ const server = new ApolloServer({
 });
 
 // integrate our Apollo server with the Express application as middleware
+async function waitServer() {
+await server.start();
 server.applyMiddleware({ app });
+}
+waitServer()
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
